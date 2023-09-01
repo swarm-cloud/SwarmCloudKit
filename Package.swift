271710bf1332c7e60b0ad967a9fe5463f1cb8cd7
snowinszu <86755838@qq.com>
2023-09-01 13:28:15 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,24 +11,34 @@ let package = Package(
     products: [
         .library(
             name: "SwarmCloudKit",
-            targets: ["SwarmCloudKit", "datachannel_wrapper"]),
+            targets: ["SwarmCloudKitWrapper"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from:"5.0.1"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from:"5.0.1")
     ],
     targets: [
+        .target(
+            name: "SwarmCloudKitWrapper",
+            dependencies: [
+                .target(name: "SwarmCloudKit"),
+                .target(name: "datachannel_wrapper"),
+                .product(name: "SwiftyJSON", package: "SwiftyJSON")
+            ]
+        ),
         .binaryTarget(
             name: "SwarmCloudKit",
-//             path: "./SwarmCloudKit.xcframework",
+//             path: "./SwarmCloudKit.xcframework"
             url: "https://github.com/swarm-cloud/SwarmCloudKit/releases/download/xcframework-3.0.0/SwarmCloudKit.xcframework.zip",
-            checksum: "5f1aa96dcb4ea1c99f04c52d61fc8f42a6b0799cc215055541a7b292db5e2243"
-            ),
+            checksum: "5f1aa96dcb4ea1c99f04c52d61fc8f42a6b0799cc215055541a7b292db5e2243",
+            
+        ),
         .binaryTarget(
             name: "datachannel_wrapper",
-//             path: "./datachannel_wrapper.xcframework",
+//             path: "./datachannel_wrapper.xcframework"
             url: "https://github.com/swarm-cloud/datachannel-native/releases/download/apple-1.0.8/datachannel_wrapper.xcframework.zip",
             checksum: "e891db14503689c873c8f666d501acea0d8a8fea18a4088975f4c77e3198d01b"
-            )
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
