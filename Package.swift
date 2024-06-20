fc07bb8c9bef97a77865c235785832460b7b61e6
snowinszu <86755838@qq.com>
2024-06-20 18:59:50 +0800

    v3.2.0

--- Package.swift
+++ Package.swift
@@ -26,14 +26,14 @@ let package = Package(
         .binaryTarget(
             name: "SwarmCloudKit",
 //             path: "./SwarmCloudKit.xcframework"
-            url: "https://web3lab.b-cdn.net/apple/3.1.2/SwarmCloudKit.xcframework.zip",
-            checksum: "74715b25031bd9199640cd54e0fbf04351cbc10deca0aa8752c2f209048256cd"
+            url: "https://web3lab.b-cdn.net/apple/3.2.0/SwarmCloudKit.xcframework.zip",
+            checksum: "30ecedc6c432a876195a3064569964ba2c6df488496ce2c2c0e3220025943433"
         ),
         .binaryTarget(
             name: "datachannel_wrapper",
 //             path: "./datachannel_wrapper.xcframework"
-            url: "https://web3lab.b-cdn.net/apple/libDC/1.0.190/datachannel_wrapper.xcframework.zip",
-            checksum: "08c0b5f92cb34df6e2cbee809833096ea0eb2d0044fabcf4cb2f02d730df5073"
+            url: "https://web3lab.b-cdn.net/apple/libDC/1.0.211/datachannel_wrapper.xcframework.zip",
+            checksum: "c7cbc55433b453eb6bc384cfda9e68adcd6e01884525c5a58f4567a415d44e06"
         )
     ],
     swiftLanguageVersions: [.v5]
